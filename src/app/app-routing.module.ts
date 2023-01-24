d8447a4415576095615d964fd8c6a6217e8c9127
Olvera Deanda <colveradeanda@deloitte.com>
2023-01-23 20:18:53 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+// Componentes
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/ingrearPresupuesto', pathMatch: 'full'},
+  {path: 'ingrearPresupuesto', component: IngresarPresupuestoComponent},
+  {path: 'gastos', component: GastosComponent},
+  {path: '**', redirectTo: '/ingrearPresupuesto', pathMatch: 'full'} // ! Ésta línea debe estar al último
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
